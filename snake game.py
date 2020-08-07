a670ae44b9dc48a9082a4eaa8ccaf887c656cabf
blabfish <69131338+blabfish@users.noreply.github.com>
2020-08-07 16:43:22 +0800

    Add files via upload

--- snake game.py
+++ snake game.py
@@ -1,101 +1,188 @@
-#snake game
-
-import math
+import turtle
+#Pen
+import time
 import random
-import pygame
-import tkinter as tk
-from tkinter import massagebox
 
-class cube(object):
-    rows = 0
-    w = 0
-    def _init_(self,start,dirnx=1,dirny=0,color=(255,0,0)):
-        pass
-    def move(self, dirnx, dirny):
-        pass
-    def draw(self, surface,eyes=False):
-        pass
+delay = 0.1
 
-class snake(object):
-    body = []
-    turns = {}
-    def _init_(self, color, pos):
-        self.color = color
-        self.head = cube(pos)
-        self.body.append(self.head)
-        self.dirnx = 0
-        self.dirny = 1
-    def move(self):
-        for evenin pygame.event.get():
-            if event.type == pygame.quit:
-                pygame.quit()
-                
-            keys = pygame.key.get_pressed()
-            
-            for key in keys:
-                if keys[pygame.K_LEFT]:
-                    self.dirnx = -1
-                    self.dirny = 0
-                    self.turns[self.head.pos[:]] = [self.dirnx,]
-                if keys[pygame.K_RIGHT]:
-                    
-                if keys[pygame.K_UP]:
-                    
-                if keys[pygame.K_DOWN]:
-    def reset(self, pos):
-        pass
-    def addCube(self):
-        pass
-    def draw(self, surface):
-        pass
-def drawGrid(w, rows, surface):
-    sizeBtwn = w // rows
-    
-    x = 0
-    y = 0
-    for i in range(rows):
-        x = x + sizeBtwn
-        y = y + sizeBtwn
+# Score
+score = 0
+high_score = 0
+
+# Set up the screen
+#wn = window
+wn = turtle.Screen()
+wn.title("Snake Game understood by Stephen Han")
+wn.bgcolor("green")
+#Backround
+wn.setup(width=600, height=600)
+wn.tracer(0) # Turns off
+
+# Snake head
+head = turtle.Turtle()
+head.speed(0)
+head.shape("square")    
+head.color("black")
+head.penup()
+head.goto(0,0)
+head.direction = "stop"
+
+# Snake food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+segments = []
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0  High Score: 0", align="center", font=("Courier", 24, "normal"))
+
+# Functions
+def go_up():
+    if head.direction != "down":
+        head.direction = "up"
+
+def go_down():
+    if head.direction != "up":
+        head.direction = "down"
+
+def go_left():
+    if head.direction != "right":
+        head.direction = "left"
+
+def go_right():
+    if head.direction != "left":
+        head.direction = "right"
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+# Keyboard bindings
+wn.listen()
+wn.onkeypress(go_up, "Up")
+wn.onkeypress(go_down, "Down")
+wn.onkeypress(go_left, "Left")
+wn.onkeypress(go_right, "Right")
+
+# Main game loop
+while True:
+    wn.update()
+
+    # Check for a collision with the border
+    if head.xcor()>290 or head.xcor()<-290 or head.ycor()>290 or head.ycor()<-290:
+        time.sleep(1)
+        head.goto(0,0)
+        head.direction = "stop"
+
+        # Hide the segments
+        for segment in segments:
+            segment.goto(1000, 1000)
         
-        pygame.draw.line(surface, (255,255,255), (x,0), (x,w))
-        pygame.draw.line(surface, (255,255,255), (0,y), (w,y))
+        # Clear the segments list
+        segments.clear()
+
+        # Reset the score
+        score = 0
+
+        # Reset the delay
+        delay = 0.1
+
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+
+
+    # Check for a collision with the food
+    if head.distance(food) < 20:
+        # Move the food to a random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x,y)
+
+        # Add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("grey")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # Shorten the delay
+        delay -= 0.001
+
+        # Increase the score
+        score += 10
+
+        if score > high_score:
+            high_score = score
         
-def redrawWindow(surface):
-    global rows, width
-    surface.fill((0,0,0))
-    drawGrid(width, rows, surface)
-    pygame.display.update()
-def randomSnack(rows, items):
-    pass
-def massage_box(subject, content):
-    pass
-def main():
-    global width, rows
-    width = 500
-    rows = 20
-    win = pygame.display.set_mode((width, width))
-    s = snake((255,0,0), (10,10))
-    flag = True
-    
-    clock = pygame.time.Clock()
-    
-    while flag:
-        pygame.time.delay(50)
-        clock.tick(10)
-        redrawWindow(win)
-    
-    pass
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+        pen.clear()
+        pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal")) 
+
+    # Move the end segments first in reverse order
+    #Body movement
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x, y)
+
+    # Move segment 0 to where the head is
+    #movement
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x,y)
+
+    move()    
+
+    # Check for head collision with the body segments
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0,0)
+            head.direction = "stop"
+        
+            # Hide the segments
+            #No more body
+            for segment in segments:
+                segment.goto(1000, 1000)
+        
+            # Clear the segments list
+            segments.clear()
+
+            # Reset the score
+            score = 0
+
+            # Reset the delay
+            delay = 0.1
+        
+            # Update the score display
+            pen.clear()
+            pen.write("Score: {}  High Score: {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
+    time.sleep(delay)
+
+wn.mainloop()
